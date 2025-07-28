bb422d5f3a6aeb9c17d632642e7443d9324ac722
yawbtng <154343001+yawbtng@users.noreply.github.com>
2025-07-28 16:20:03 -0700

    project set up

---
+++ browser-app-demo/stagehand.config.ts
@@ -0,0 +1,42 @@
+import type { ConstructorParams } from "@browserbasehq/stagehand";
+import dotenv from "dotenv";
+import { AISdkClient } from "./llm_clients/aisdk_client.js";
+import { openai } from "@ai-sdk/openai";
+
+dotenv.config();
+
+const StagehandConfig: ConstructorParams = {
+  verbose: 1 /* Verbosity level for logging: 0 = silent, 1 = info, 2 = all */,
+  domSettleTimeoutMs: 30_000 /* Timeout for DOM to settle in milliseconds */,
+
+  // LLM configuration
+
+  llmClient: new AISdkClient({
+    model: openai("gpt-4o-mini"),
+  }),
+
+  // Browser configuration
+  env: "LOCAL" /* Environment to run in: LOCAL or BROWSERBASE */,
+  apiKey: process.env.BROWSERBASE_API_KEY /* API key for authentication */,
+  projectId: process.env.BROWSERBASE_PROJECT_ID /* Project identifier */,
+  browserbaseSessionID:
+    undefined /* Session ID for resuming Browserbase sessions */,
+  browserbaseSessionCreateParams: {
+    projectId: process.env.BROWSERBASE_PROJECT_ID!,
+    browserSettings: {
+      blockAds: true,
+      viewport: {
+        width: 1024,
+        height: 768,
+      },
+    },
+  },
+  localBrowserLaunchOptions: {
+    viewport: {
+      width: 1024,
+      height: 768,
+    },
+  } /* Configuration options for the local browser */,
+};
+
+export default StagehandConfig;
